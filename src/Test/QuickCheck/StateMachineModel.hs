e818ea236e427f42bbb10f4c6156c034d3bbc409
Stevan Andjelkovic <stevan@advancedtelematic.com>
2017-02-15 15:09:43 +0100

    Add library and tests.

---
+++ src/Test/QuickCheck/StateMachineModel.hs
@@ -0,0 +1,451 @@
+{-# LANGUAGE DeriveFunctor         #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+
+module Test.QuickCheck.StateMachineModel where
+
+import           Control.Concurrent                  (threadDelay)
+import           Control.Concurrent.ParallelIO.Local (parallel_, withPool)
+import           Control.Concurrent.STM.TChan        (TChan, newTChanIO,
+                                                      tryReadTChan, writeTChan)
+import           Control.Monad.State
+import           Control.Monad.STM                   (STM, atomically)
+import           Data.Char                           (toLower)
+import           Data.Foldable                       (toList)
+import           Data.List                           (partition)
+import           Data.Monoid                         ((<>))
+import qualified Data.Set                            as Set
+import           System.Random                       (randomRIO)
+import           Test.QuickCheck
+import           Test.QuickCheck.Monadic
+import           Test.QuickCheck.Property            (Property (..))
+import           Text.PrettyPrint.ANSI.Leijen        (Pretty, align, dot,
+                                                      indent, int, pretty,
+                                                      prettyList, text,
+                                                      underline, vsep, (<+>))
+
+------------------------------------------------------------------------
+
+forAllShrinkShow
+  :: Testable prop
+  => Gen a -> (a -> [a]) -> (a -> String) -> (a -> prop) -> Property
+forAllShrinkShow gen shrinker shower pf =
+  again $
+  MkProperty $
+  gen >>= \x ->
+    unProperty $
+    shrinking shrinker x $ \x' ->
+      counterexample (shower x') (pf x')
+
+assert' :: Monad m => String -> Bool -> PropertyM m ()
+assert' _   True  = return ()
+assert' msg False = fail msg
+
+------------------------------------------------------------------------
+
+liftSem
+  :: (MonadState [ref] m, RefKit cmd ref')
+  => (cmd ref -> m resp)
+  -> (resp -> Maybe ref)
+  -> (cmd ref' -> m resp)
+liftSem sem isRef cmd
+  | returnsRef cmd = do
+      env <- get
+      resp <- sem (fmap (\ref -> env !! fromEnum ref) cmd)
+      case isRef resp of
+        Nothing  -> error "liftSem: response wasn't a ref."
+        Just ref -> do
+          modify (++ [ref])
+          return resp
+  | otherwise      = do
+      env <- get
+      sem (fmap (\ref -> env !! fromEnum ref) cmd)
+
+------------------------------------------------------------------------
+
+liftGen
+  :: forall cmd ref
+  .  RefKit cmd ()
+  => Enum ref
+  => [(Int, Gen (cmd ()))] -> Int -> Gen ([cmd ref], Int)
+liftGen gens n = sized $ \sz -> runStateT (go sz) n
+  where
+  go :: Int -> StateT Int Gen [cmd ref]
+  go 0  = return []
+  go sz = do
+    scope <- get
+    cmd   <- lift $ frequency gens
+    if scope == 0
+    then do
+      if returnsRef cmd && null (usesRefs cmd)
+      then do
+        put 1
+        ih <- go (sz - 1)
+        return $ fmap undefined cmd : ih
+      else go sz
+    else do
+      when (returnsRef cmd) $
+        modify succ
+      ih <- go (sz - 1)
+      if null $ usesRefs cmd
+      then
+        return $ fmap undefined cmd : ih
+      else do
+        ref <- toEnum <$> lift (choose (0, scope - 1))
+        return $ fmap (const ref) cmd : ih
+
+------------------------------------------------------------------------
+
+liftShrink
+  :: RefKit cmd ref
+  => (cmd ref -> [cmd ref]) -> [cmd ref] -> [[cmd ref]]
+liftShrink shrinker = liftShrink' 0 shrinker
+
+liftShrink'
+  :: RefKit cmd ref
+  => Int -> (cmd ref -> [cmd ref]) -> [cmd ref] -> [[cmd ref]]
+liftShrink' n0 shrinker = go n0
+  where
+  go _ []       = []
+  go n (c : cs) =
+    [ [] ] ++
+    [ fixRefs n c cs ] ++
+    [ c' : cs' | (c', cs') <- shrinkPair' shrinker (go n') (c, cs) ]
+    where
+    n' = if returnsRef c then n + 1 else n
+
+------------------------------------------------------------------------
+
+monadicOur :: env -> PropertyM (StateT env IO) () -> Property
+monadicOur env0 = monadic $ ioProperty . flip evalStateT env0
+
+sequentialProperty
+  :: forall cmd ref ref' model resp
+  .  Show (cmd ref)
+  => RefKit cmd ref
+  => RefKit cmd ()
+  => (model -> cmd ref -> Bool)
+  -> (model -> cmd ref -> resp -> Maybe model)
+  -> model
+  -> [(Int, Gen (cmd ()))]
+  -> (cmd ref -> [cmd ref])
+  -> ([cmd ref] -> String)
+  -> (cmd ref' -> StateT [ref'] IO resp)
+  -> (resp -> Maybe ref')
+  -> [ref']
+  -> Property
+sequentialProperty preConds postNext model0 gens shrinker shower runCmd isRef env0 =
+  forAllShrinkShow
+    (fst <$> liftGen gens 0)
+    (liftShrink shrinker)
+    shower $
+    monadicOur env0 . go model0
+  where
+  go :: model -> [cmd ref] -> PropertyM (StateT [ref'] IO) ()
+  go _ []           = return ()
+  go m (cmd : cmds) = do
+    let s = map toLower $ takeWhile (/= ' ') $ show cmd
+    monitor $ collect s
+    pre $ preConds m cmd
+    resp <- run $ runCmd' cmd
+    case postNext m cmd resp of
+      Nothing -> assert' ("`" ++ s ++ "_post'") False
+      Just m' -> go m' cmds
+    where
+    runCmd' = liftSem runCmd isRef
+
+------------------------------------------------------------------------
+
+data Fork a = Fork a a a
+  deriving (Eq, Functor, Show, Ord, Read)
+
+instance Pretty a => Pretty (Fork a) where
+  pretty (Fork l p r) = vsep
+    [ underline $ text "Prefix:"
+    , indent 5 $ pretty p
+    , underline $ text "Parallel:"
+    , indent 2 $ int 1 <> dot <+> align (pretty l)
+    , indent 2 $ int 2 <> dot <+> align (pretty r)
+    ]
+
+------------------------------------------------------------------------
+
+liftGenFork
+  :: RefKit cmd ()
+  => Enum ref
+  => [(Int, Gen (cmd ()))] -> Gen (Fork [cmd ref])
+liftGenFork gens = do
+  (prefix, n) <- liftGen gens 0
+  left        <- fst <$> liftGen gens n
+  right       <- fst <$> liftGen gens n
+  return $ Fork left prefix right
+
+------------------------------------------------------------------------
+
+liftShrinkFork
+  :: forall cmd ref
+  .  (RefKit cmd ref, Ord (cmd ref))
+  => (cmd ref -> [cmd ref]) -> Fork [cmd ref] -> [Fork [cmd ref]]
+liftShrinkFork shrinker f@(Fork l0 p0 r0) = Set.toList $ Set.fromList $
+
+  -- Only shrink the branches:
+  [ Fork l' p0 r'
+  | (l', r') <- shrinkPair (liftShrink' (countRefReturns p0) shrinker) (l0, r0)
+  ] ++
+
+  -- Only shrink the prefix:
+  shrinkPrefix f
+
+  where
+  shrinkPrefix
+    :: RefKit cmd ref
+    => Fork [cmd ref] -> [Fork [cmd ref]]
+  shrinkPrefix = go 0
+    where
+    go _ (Fork _ []       _) = []
+    go n (Fork l (p : ps) r) =
+      [ Fork l'   []               r'   ] ++
+      [ Fork l''  (fixRefs n p ps) r''  ] ++
+      [ Fork l''' (p' : ps')       r'''
+      | (p', Fork l''' ps' r''') <- shrinkPair' shrinker (go n') (p, Fork l ps r)
+      ]
+      where
+      l'  = fixManyRefs n (p : ps) l
+      r'  = fixManyRefs n (p : ps) r
+
+      l'' = fixRefs n p l
+      r'' = fixRefs n p r
+
+      n'  | returnsRef p = n + 1
+          | otherwise    = n
+
+------------------------------------------------------------------------
+
+shrinkPair :: (a -> [a]) -> (a, a) -> [(a, a)]
+shrinkPair shrinker = shrinkPair' shrinker shrinker
+
+shrinkPair' :: (a -> [a]) -> (b -> [b]) -> (a, b) -> [(a, b)]
+shrinkPair' shrinkerA shrinkerB (x, y) =
+  [ (x', y) | x' <- shrinkerA x ] ++
+  [ (x, y') | y' <- shrinkerB y ]
+
+------------------------------------------------------------------------
+
+type History cmd resp = [HistoryEvent cmd resp]
+
+data HistoryEvent cmd resp
+  = InvocationEvent { invocation   :: cmd
+                    , getProcessId :: Int
+                    }
+  | ResponseEvent   { response     :: resp
+                    , getProcessId :: Int
+                    }
+  deriving (Eq, Show, Read)
+
+data Rose a = Rose a [Rose a]
+  deriving Show
+
+fromRose :: Rose a -> [a]
+fromRose (Rose x xs) = x : concatMap fromRose xs
+
+takeInvocations :: History cmd resp -> [HistoryEvent cmd resp]
+takeInvocations = takeWhile $ \h -> case h of
+  InvocationEvent _ _ -> True
+  _                   -> False
+
+findCorrespondingResp :: Int -> History cmd resp -> [(resp, History cmd resp)]
+findCorrespondingResp _   [] = []
+findCorrespondingResp pid (ResponseEvent r pid' : es) | pid == pid' = [(r, es)]
+findCorrespondingResp pid (e : es) =
+  [ (res, e : es') | (res, es') <- findCorrespondingResp pid es ]
+
+data Operation cmd resp = Operation cmd resp Int
+  deriving Show
+
+linearTree :: (Eq cmd, Eq resp) => History cmd resp -> [Rose (Operation cmd resp)]
+linearTree [] = []
+linearTree es =
+  [ Rose (Operation cmd resp pid) (linearTree es')
+  | e@(InvocationEvent cmd pid) <- takeInvocations es
+  , (resp, es')  <- findCorrespondingResp pid $ filter1 (/= e) es
+  ]
+  where
+  filter1 :: (a -> Bool) -> [a] -> [a]
+  filter1 _ []                   = []
+  filter1 p (x : xs) | p x       = x : filter1 p xs
+                     | otherwise = xs
+
+linearise
+  :: forall cmd resp model
+  .  Eq cmd
+  => Eq resp
+  => model
+  -> (model -> cmd -> resp -> Maybe model)
+  -> History cmd resp
+  -> [[Operation cmd resp]]
+linearise m0 postNext = map fromRose . filter (postConditionsHold m0) . linearTree
+  where
+  postConditionsHold :: model -> Rose (Operation cmd resp) -> Bool
+  postConditionsHold m (Rose (Operation cmd resp _) roses) = case postNext m cmd resp of
+    Nothing -> False
+    Just m' -> any' (postConditionsHold m') roses
+    where
+    any' :: (a -> Bool) -> [a] -> Bool
+    any' _ [] = True
+    any' p xs = any p xs
+
+------------------------------------------------------------------------
+
+instance (Show cmd, Show resp) => Pretty (Operation cmd resp) where
+  pretty (Operation cmd resp _) = text (show cmd) <+> text "-->" <+> text (show resp)
+  prettyList                    = vsep . map pretty
+
+toForkOfOps :: (Eq cmd, Eq resp) => History cmd resp -> Fork [Operation cmd resp]
+toForkOfOps h = Fork (mkOps l) (mkOps p) (mkOps r)
+  where
+  (p, h') = partition (\e -> getProcessId e == 0) h
+  (l, r)  = partition (\e -> getProcessId e == 1) h'
+
+  mkOps :: [HistoryEvent cmd resp] -> [Operation cmd resp]
+  mkOps [] = []
+  mkOps (InvocationEvent cmd _ : ResponseEvent resp pid : es)
+    = Operation cmd resp pid : mkOps es
+  mkOps _ = error "mkOps: Impossible."
+
+------------------------------------------------------------------------
+
+data HistoryKit cmd resp = HistoryKit
+  { getHistoryChannel :: TChan (HistoryEvent cmd resp)
+  , getProcessId'     :: Int
+  }
+
+mkHistoryKit :: Int -> IO (HistoryKit cmd resp)
+mkHistoryKit pid = do
+  chan <- newTChanIO
+  return $ HistoryKit chan pid
+
+runMany
+  :: HistoryKit cmd resp
+  -> env
+  -> (cmd -> StateT env IO resp)
+  -> [cmd]
+  -> IO env
+runMany _   env _    []           = return env
+runMany kit env step (cmd : cmds) = do
+  atomically $ writeTChan (getHistoryChannel kit) $
+    InvocationEvent cmd (getProcessId' kit)
+  (resp, env') <- runStateT (step cmd) env
+  threadDelay =<< randomRIO (0, 20)
+  atomically $ writeTChan (getHistoryChannel kit) $
+    ResponseEvent  resp (getProcessId' kit)
+  runMany kit env' step cmds
+
+parallelProperty
+  :: forall cmd ref ref' resp model
+  .  (Ord (cmd ref), Show (cmd ref))
+  => (Eq resp, Show resp)
+  => RefKit cmd ref
+  => RefKit cmd ()
+  => (model -> cmd ref -> resp -> Maybe model)
+  -> model
+  -> [(Int, Gen (cmd ()))]
+  -> (cmd ref -> [cmd ref])
+  -> (cmd ref' -> StateT [ref'] IO resp)
+  -> (resp -> Maybe ref')
+  -> [ref']
+  -> Property
+parallelProperty postNext m0 gen shrinker runStep isRef e0
+  = forAllShrinkShow (liftGenFork gen) (liftShrinkFork shrinker) show
+  $ monadicIO
+  . \(Fork left prefix right) -> do
+      pre $ scopeCheck $ prefix <> left
+      pre $ scopeCheck $ prefix <> right
+      replicateM_ 10 $ do
+        kit <- run $ mkHistoryKit 0
+        e <- run $ runMany kit e0 runStep' prefix
+        run $ withPool 2 $ \pool -> do
+          parallel_ pool [ runMany (kit { getProcessId' = 1}) e runStep' left
+                         , runMany (kit { getProcessId' = 2}) e runStep' right
+                         ]
+
+        hist <- run $ getChanContents $ getHistoryChannel kit
+
+        if null hist
+        then return ()
+        else do
+          let lin = linearise m0 postNext hist
+          when (null lin) $ do
+            monitor $ counterexample $ show $ pretty $ toForkOfOps hist
+          assert' "Couldn't linearise" $ not $ null lin
+  where
+  runStep' :: cmd ref -> StateT [ref'] IO resp
+  runStep' = liftSem runStep isRef
+
+  getChanContents :: forall a. TChan a -> IO [a]
+  getChanContents chan = do
+    xs <- atomically $ go []
+    return $ reverse xs
+    where
+    go :: [a] -> STM [a]
+    go acc = do
+      mx <- tryReadTChan chan
+      case mx of
+        Just x  -> go $ x : acc
+        Nothing -> return acc
+
+------------------------------------------------------------------------
+
+class (Functor cmd, Foldable cmd,
+       Enum ref, Ord ref) => RefKit cmd ref where
+  returnsRef :: cmd ref -> Bool
+
+  usesRefs :: cmd ref -> [ref]
+  usesRefs = toList
+
+countRefReturns :: RefKit cmd ref => [cmd ref] -> Int
+countRefReturns = length . filter returnsRef
+
+scopeCheck
+  :: forall cmd ref
+  .  RefKit cmd ref
+  => [cmd ref]
+  -> Bool
+scopeCheck = go 0
+  where
+  go :: Int -> [cmd ref] -> Bool
+  go _ []       = True
+  go s (c : cs) = all (\r -> r < toEnum s) (usesRefs c) &&
+    go (if returnsRef c then s + 1 else s) cs
+
+fixRefs :: RefKit cmd ref => Int -> cmd ref -> [cmd ref] -> [cmd ref]
+fixRefs n c cs
+  | returnsRef c = map (fmap (\ref -> if r < ref then toEnum (fromEnum ref - 1) else ref))
+                 . filter (\ms -> [r] /= usesRefs ms)
+                 $ cs
+  | otherwise    = cs
+  where
+  r = toEnum n
+
+fixManyRefs :: RefKit cmd ref => Int -> [cmd ref] -> [cmd ref] -> [cmd ref]
+fixManyRefs _ []       ds = ds
+fixManyRefs n (c : cs) ds = fixManyRefs n cs (fixRefs n c ds)
+
+------------------------------------------------------------------------
+
+ppFork :: Show (cmd ref) => Fork [cmd ref] -> IO ()
+ppFork = putStrLn . ppFork'
+
+ppFork' :: Show (cmd ref) => Fork [cmd ref] -> String
+ppFork' = show . pretty . fmap (prettyList . map show)
+
+ppForks :: Show (cmd ref) => [Fork [cmd ref]] -> IO ()
+ppForks = mapM_ putStrLn . lines . ppForks'
+
+ppForks' :: Show (cmd ref) => [Fork [cmd ref]] -> String
+ppForks' = unlines . map (show . pretty . fmap (prettyList . map show))
+
+debugShrink
+  :: (Show (cmd ref), RefKit cmd ref, Ord (cmd ref))
+  => (cmd ref -> [cmd ref]) -> Fork [cmd ref] -> IO ()
+debugShrink shrinker = mapM_ ppFork . liftShrinkFork shrinker
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,365 @@
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+
+module Main where
+
+import           Control.Concurrent                (threadDelay)
+import           Control.Monad.State
+import           Data.Char
+import           Data.IORef
+import           Data.List
+import           System.Random
+import           Test.Hspec
+import           Test.QuickCheck
+import           Test.QuickCheck.Monadic
+
+import           Test.QuickCheck.StateMachineModel
+
+------------------------------------------------------------------------
+
+newtype Ref = Ref Int
+  deriving (Eq, Read, Ord, Show, Enum)
+
+unRef :: Ref -> Int
+unRef (Ref i) = i
+
+data MemStepF ref
+  = New
+  | Read ref
+  | Write ref Int
+  | Inc ref
+  deriving (Show, Eq, Read, Functor, Foldable, Ord)
+
+type MemStep = MemStepF Ref
+
+newtype Mem = Mem { unMem :: [MemStep] }
+  deriving (Show, Eq, Monoid, Read)
+
+------------------------------------------------------------------------
+
+type Model = [Int]
+
+initModel :: Model
+initModel = []
+
+new_pre :: Model -> Bool
+new_pre _ = True
+
+new_next :: Model -> () -> IORef Int -> Model
+new_next m _ _ = m ++ [0]
+
+new_post :: Model -> () -> IORef Int -> Bool
+new_post _ _ _ = True
+
+
+read_pre :: Model -> Ref -> Bool
+read_pre m (Ref i) = i < length m
+
+read_next :: Model -> Ref -> Int -> Model
+read_next m _ _ = m
+
+(!) :: [a] -> Ref -> a
+xs0 ! (Ref i0) = case go xs0 i0 of
+  Nothing -> error $ "!: " ++ show (length xs0, i0)
+  Just x  -> x
+  where
+  go :: [a] -> Int -> Maybe a
+  go []       _ = Nothing
+  go (x : _)  0 = Just x
+  go (_ : xs) i = go xs (i - 1)
+
+
+read_post :: Model -> Ref -> Int -> Bool
+read_post m ref r = m ! ref == r && m' == m
+  where
+  m' = read_next m ref r
+
+write_pre :: Model -> Ref -> Bool
+write_pre m (Ref i) = i < length m
+
+update :: [a] -> Int -> a -> [a]
+update []       _ _ = []
+update (_ : xs) 0 y = y : xs
+update (x : xs) i y = x : update xs (i - 1) y
+
+write_next :: Model -> (Ref, Int) -> () -> Model
+write_next m (ref, i) _ = update m (unRef ref) i
+
+write_post :: Model -> (Ref, Int) -> () -> Bool
+write_post m (ref, i) _ = m' ! ref == i
+  where
+  m' = write_next m (ref, i) ()
+
+inc_pre :: Model -> Ref -> Bool
+inc_pre m (Ref i) = i < length m
+
+inc_next :: Model -> Ref -> () -> Model
+inc_next m ref _ = update m (unRef ref) ((m ! ref) + 1)
+
+inc_post :: Model -> Ref -> () -> Bool
+inc_post m ref _ = m' ! ref == m ! ref + 1
+  where
+  m' = inc_next m ref ()
+
+------------------------------------------------------------------------
+
+data Response
+  = NewR Int (IORef Int)
+  | ReadR Int
+  | WriteR
+  | IncR
+  deriving Eq
+
+instance Show Response where
+  show (NewR i _) = '$' : show i
+  show (ReadR i)  = show i
+  show WriteR     = "()"
+  show IncR       = "()"
+
+------------------------------------------------------------------------
+
+type OurMonad m = (MonadIO m, MonadState Env m)
+
+type Env = [IORef Int]
+
+defaultEnv :: Env
+defaultEnv = []
+
+data Problem = None | Bug | RaceCondition
+  deriving Eq
+
+semStep
+  :: (MonadIO m, MonadState Env m)
+  => Problem
+  -> MemStepF (IORef Int)
+  -> m Response
+semStep _   New           = do
+  i   <- length <$> get
+  ref <- liftIO (newIORef 0)
+  return $ NewR i ref
+semStep _   (Read ref)    = ReadR  <$> liftIO (readIORef ref)
+semStep prb (Write ref i) = WriteR <$  liftIO (writeIORef ref i')
+  where
+  -- Introduce bug:
+  i' | i `elem` [5..10] = if prb == Bug then i + 1 else i
+     | otherwise        = i
+semStep prb (Inc ref)     = do
+  liftIO $ do
+
+    -- Possible race condition:
+    if prb == RaceCondition
+    then do
+      i <- readIORef ref
+      threadDelay =<< randomRIO (0, 200)
+      writeIORef ref (i + 1)
+    else
+      modifyIORef' ref (+ 1)
+
+  return IncR
+
+isRef :: Response -> Maybe (IORef Int)
+isRef (NewR _ ref) = Just ref
+isRef _            = Nothing
+
+debugMem :: Mem -> IO ()
+debugMem m = do
+  putStrLn ""
+  (_, env) <- flip runStateT defaultEnv $ sem m
+  putStrLn ""
+  forM_ (zip [(0 :: Integer)..] env) $ \(i, ref) -> do
+    v <- readIORef ref
+    putStrLn $ "$" ++ show i ++ ": " ++ show v
+  where
+  sem :: OurMonad m => Mem -> m ()
+  sem = foldM (\ih ms -> semStep' ms >> return ih) () . unMem
+    where
+    semStep' = liftSem (semStep None) isRef
+
+------------------------------------------------------------------------
+
+gens :: [(Int, Gen (MemStepF ()))]
+gens =
+  [ (1, return New)
+  , (5, return $ Read ())
+  , (5, Write <$> pure () <*> arbitrary)
+  , (5, return $ Inc ())
+  ]
+
+------------------------------------------------------------------------
+
+shrink1 :: MemStepF ref -> [MemStepF ref]
+shrink1 (Write ref i) = [ Write ref i' | i' <- shrink i ]
+shrink1 _             = []
+
+------------------------------------------------------------------------
+
+instance (Enum ref, Ord ref) => RefKit MemStepF ref where
+  returnsRef New = True
+  returnsRef _   = False
+
+preConds :: Model -> MemStep -> Bool
+preConds m New           = new_pre   m
+preConds m (Read ref)    = read_pre  m ref
+preConds m (Write ref _) = write_pre m ref
+preConds m (Inc ref)     = inc_pre   m ref
+
+postNext :: Model -> MemStepF Ref -> Response -> Maybe Model
+postNext m New           (NewR _ ref)
+  | new_post m () ref = Just $ new_next m () ref
+  | otherwise         = Nothing
+postNext m (Read ref)    (ReadR i)
+  | read_post m ref i = Just $ read_next m ref i
+  | otherwise         = Nothing
+postNext m (Write ref i) WriteR
+  | write_post m (ref, i) () = Just $ write_next m (ref, i) ()
+  | otherwise         = Nothing
+postNext m (Inc ref)     IncR
+  | inc_post m ref () = Just $ inc_next m ref ()
+  | otherwise         = Nothing
+postNext _ _ _ = error "postNext: Impossible"
+
+------------------------------------------------------------------------
+
+prop_safety :: Problem -> Property
+prop_safety prb = sequentialProperty
+  preConds
+  postNext
+  initModel
+  gens
+  shrink1
+  show
+  (semStep prb)
+  isRef
+  defaultEnv
+
+prop_parallel :: Problem -> Property
+prop_parallel prb = parallelProperty
+  postNext
+  initModel
+  gens
+  shrink1
+  (semStep prb)
+  isRef
+  defaultEnv
+
+------------------------------------------------------------------------
+
+prop_genScope :: Property
+prop_genScope = forAll (liftGenFork gens) $ \(Fork l p r) ->
+  scopeCheck (p ++ l) &&
+  scopeCheck (p ++ (r :: [MemStep]))
+
+shrinkPropertyHelper :: Property -> (String -> Bool) -> Property
+shrinkPropertyHelper prop p = monadicIO $ do
+  result <- run $ quickCheckWithResult (stdArgs {chatty = False}) prop
+  case result of
+    Failure { output = outputLines } -> do
+      assert' outputLines $ p outputLines
+    _                                -> return ()
+
+prop_sequentialShrink :: Property
+prop_sequentialShrink = shrinkPropertyHelper (prop_safety Bug)
+  ((== "[New,Write (Ref 0) 5,Read (Ref 0)]") . last . lines)
+
+cheat :: Fork [MemStep] -> Fork [MemStep]
+cheat = fmap (map (\ms -> case ms of
+                      Write ref _ -> Write ref 0
+                      _           -> ms))
+
+prop_shrinkForkSubseq :: Property
+prop_shrinkForkSubseq = forAll (liftGenFork gens) $ \f@(Fork l p r) ->
+  all (\(Fork l' p' r') -> noRefs l' `isSubsequenceOf` noRefs l &&
+                           noRefs p' `isSubsequenceOf` noRefs p &&
+                           noRefs r' `isSubsequenceOf` noRefs r)
+      (liftShrinkFork shrink1 (cheat f))
+
+  where
+  noRefs = fmap (const ())
+
+prop_shrinkForkScope :: Property
+prop_shrinkForkScope = forAll (liftGenFork gens) $ \f ->
+  all (\(Fork l' p' r') -> scopeCheck (p' ++ l') &&
+                           scopeCheck (p' ++ (r' :: [MemStep])))
+      (liftShrinkFork shrink1 f)
+
+------------------------------------------------------------------------
+
+prop_shrinkForkMinimal :: Property
+prop_shrinkForkMinimal = shrinkPropertyHelper (prop_parallel RaceCondition) $ \out ->
+  let f = read $ dropWhile isSpace (lines out !! 1)
+  in hasMinimalShrink f || f `elem` minimal
+  where
+  hasMinimalShrink :: Fork [MemStep] -> Bool
+  hasMinimalShrink
+    = anyRose (`elem` minimal)
+    . rose (liftShrinkFork shrink1)
+    where
+    anyRose :: (a -> Bool) -> Rose a -> Bool
+    anyRose p (Rose x xs) = p x || any (anyRose p) xs
+
+    rose :: (a -> [a]) -> a -> Rose a
+    rose more = go
+      where
+      go x = Rose x $ map go $ more x
+
+  minimal :: [Fork [MemStep]]
+  minimal  = minimal' ++ map mirrored minimal'
+    where
+    minimal' = [m0, m1, m2, m3]
+
+    mirrored :: Fork a -> Fork a
+    mirrored (Fork l p r) = Fork r p l
+
+    m0 = Fork [Write (Ref 0) 0, Read (Ref 0)]
+              [New]
+              [Inc (Ref 0)]
+
+    m1 = Fork [Write (Ref 0) 0]
+              [New]
+              [Inc (Ref 0), Read (Ref 0)]
+
+    m2 = Fork [Inc (Ref 0),Read (Ref 0)]
+              [New]
+              [Inc (Ref 0)]
+
+    m3 = Fork [Inc (Ref 0)]
+              [New]
+              [Inc (Ref 0), Read (Ref 0)]
+
+------------------------------------------------------------------------
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+  describe "liftGenFork" $
+    it "generates well-scoped programs"
+      prop_genScope
+
+  describe "sequentialProperty" $ do
+
+    it "returns a property that passes when there are no bugs" $
+      prop_safety None
+
+    it "always shrinks to the minimal counterexample when there's a bug"
+      prop_sequentialShrink
+
+  describe "liftShrinkFork" $ do
+
+    it "shrinks into subsequences"
+      prop_shrinkForkSubseq
+
+    it "shrinks into well-scoped programs"
+      prop_shrinkForkScope
+
+  describe "parallelProperty" $ do
+
+    xit "returns a property that passes when there are no race conditions" $ do
+      prop_parallel None
+
+    it "always shrinks to one of the minimal counter examples when there's a race condition"
+      prop_shrinkForkMinimal
