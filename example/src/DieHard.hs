bce0868405e8037532b5d54200d4485b0417daa9
Stevan Andjelkovic <stevan@advancedtelematic.com>
2017-05-12 14:26:04 +0200

    Move Die Hard meta-properties to spec module.

--- example/src/DieHard.hs
+++ example/src/DieHard.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE DataKinds          #-}
-{-# LANGUAGE ExplicitNamespaces #-}
 {-# LANGUAGE FlexibleInstances  #-}
 {-# LANGUAGE GADTs              #-}
 {-# LANGUAGE PolyKinds          #-}
 {-# LANGUAGE Rank2Types         #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators      #-}
 
-module DieHard where
+module DieHard
+  ( Step(..)
+  , State(..)
+  , initState
+  , transitions
+  , prop_dieHard
+  ) where
 
 import           Control.Monad.Identity  (Identity, runIdentity)
-import           Data.List
 import           Data.Singletons.Prelude (ConstSym1, TyFun)
 import           Test.QuickCheck         (Gen, Property, label, property)
 
@@ -27,8 +30,6 @@ data Step :: Response () -> (TyFun () * -> *) -> * where
   EmptySmall   :: Step ('Response ()) refs
   SmallIntoBig :: Step ('Response ()) refs
   BigIntoSmall :: Step ('Response ()) refs
-
-deriving instance Show (Step resp refs)
 
 ------------------------------------------------------------------------
 
@@ -69,12 +70,12 @@ smm = StateMachineModel preconditions postconditions transitions initState
 
 gens :: [(Int, Gen (Untyped Step (IxRefs ())))]
 gens =
-  [ (1, return . Untyped $ FillBig)
-  , (1, return . Untyped $ FillSmall)
-  , (1, return . Untyped $ EmptyBig)
-  , (1, return . Untyped $ EmptySmall)
-  , (1, return . Untyped $ SmallIntoBig)
-  , (1, return . Untyped $ BigIntoSmall)
+  [ (1, return $ Untyped FillBig)
+  , (1, return $ Untyped FillSmall)
+  , (1, return $ Untyped EmptyBig)
+  , (1, return $ Untyped EmptySmall)
+  , (1, return $ Untyped SmallIntoBig)
+  , (1, return $ Untyped BigIntoSmall)
   ]
 
 shrink1 :: Untyped' Step refs -> [Untyped' Step refs ]
@@ -89,71 +90,6 @@ semStep EmptyBig     = return ()
 semStep EmptySmall   = return ()
 semStep SmallIntoBig = return ()
 semStep BigIntoSmall = return ()
-
-------------------------------------------------------------------------
-
-prop_dieHard :: Property
-prop_dieHard = sequentialProperty
-  smm
-  gens
-  shrink1
-  returns
-  semStep
-  runIdentity
-
-validSolutions :: [[Step ('Response ()) (ConstSym1 ())]]
-validSolutions =
-  [ [ FillBig
-    , BigIntoSmall
-    , EmptySmall
-    , BigIntoSmall
-    , FillBig
-    , BigIntoSmall
-    ]
-  , [ FillSmall
-    , SmallIntoBig
-    , FillSmall
-    , SmallIntoBig
-    , EmptyBig
-    , SmallIntoBig
-    , FillSmall
-    , SmallIntoBig
-    ]
-  , [ FillSmall
-    , SmallIntoBig
-    , FillSmall
-    , SmallIntoBig
-    , EmptySmall
-    , BigIntoSmall
-    , EmptySmall
-    , BigIntoSmall
-    , FillBig
-    , BigIntoSmall
-    ]
-  , [ FillBig
-    , BigIntoSmall
-    , EmptyBig
-    , SmallIntoBig
-    , FillSmall
-    , SmallIntoBig
-    , EmptyBig
-    , SmallIntoBig
-    , FillSmall
-    , SmallIntoBig
-    ]
-  ]
-
-testValidSolutions :: Bool
-testValidSolutions = all ((/= 4) . bigJug . run) validSolutions
-  where
-  run = foldr (\c s -> transitions s c ()) initState
-
-prop_bigJug4 :: Property
-prop_bigJug4 = shrinkPropertyHelper' prop_dieHard $ \output ->
-  let counterExample = lines output !! 1 in
-  case find (== counterExample) (map show validSolutions) of
-    Nothing -> property False
-    Just ex -> label ex (property True)
 
 ------------------------------------------------------------------------
 
@@ -176,8 +112,6 @@ instance IxTraversable Step where
   ifor _ SmallIntoBig _ = pure SmallIntoBig
   ifor _ BigIntoSmall _ = pure BigIntoSmall
 
-deriving instance Eq (Step resp ConstIntRef)
-
 instance IxFunctor Step where
   ifmap _ FillBig      = FillBig
   ifmap _ FillSmall    = FillSmall
@@ -193,3 +127,14 @@ instance Show (Untyped' Step ConstIntRef) where
   show (Untyped' EmptySmall   _) = "EmptySmall"
   show (Untyped' SmallIntoBig _) = "SmallIntoBig"
   show (Untyped' BigIntoSmall _) = "BigIntoSmall"
+
+------------------------------------------------------------------------
+
+prop_dieHard :: Property
+prop_dieHard = sequentialProperty
+  smm
+  gens
+  shrink1
+  returns
+  semStep
+  runIdentity
