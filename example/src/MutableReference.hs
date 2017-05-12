86731ee898a337b0ccbe9c8bc934dac7b2fc73f4
Stevan Andjelkovic <stevan@advancedtelematic.com>
2017-05-12 11:15:56 +0200

    Move meta-properties about mutable refs to its spec module.

--- example/src/MutableReference.hs
+++ example/src/MutableReference.hs
@@ -1,44 +1,31 @@
-{-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFoldable             #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE PolyKinds                  #-}
-{-# LANGUAGE Rank2Types                 #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE TypeInType                 #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE ExplicitNamespaces    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE KindSignatures        #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module MutableReference where
 
-import           Control.Concurrent                    (threadDelay)
-import           Control.Monad.State
-import           Data.Char
-import           Data.Constraint
-import           Data.Dynamic
-import           Data.IORef
-import           Data.Kind
-import           Data.List                             hiding ((\\))
-import           Data.Map                              (Map)
-import qualified Data.Map                              as M
-import           Data.Singletons.Prelude               hiding ((:-), Map)
-import           System.Random
-import           Test.QuickCheck
-import           Text.ParserCombinators.ReadP          (string)
-import           Text.Read
+import           Control.Concurrent      (threadDelay)
+import           Control.Monad.IO.Class  (MonadIO, liftIO)
+import           Data.Constraint         ((\\))
+import           Data.IORef              (IORef, atomicModifyIORef', newIORef,
+                                          readIORef, writeIORef)
+import           Data.Map                (Map)
+import qualified Data.Map                as M
+import           Data.Singletons.Prelude (type (@@), ConstSym1, Proxy (..),
+                                          Sing (STuple0), TyFun)
+import           System.Random           (randomRIO)
+import           Test.QuickCheck         (Gen, Property, arbitrary, ioProperty,
+                                          property, shrink)
 
 import           Test.StateMachine
-import           Test.StateMachine.Internal.Parallel
-import           Test.StateMachine.Internal.Sequential
 import           Test.StateMachine.Types
-import           Test.StateMachine.Types.AlphaEquality
 import           Test.StateMachine.Utils
 
 ------------------------------------------------------------------------
@@ -68,11 +55,11 @@ preconditions (Model m) cmd = (case cmd of
 transitions :: forall refs resp. IxForallF Ord refs => Model refs -> MemStep resp refs
   -> Response_ refs resp -> Model refs
 transitions (Model m) cmd resp = (case cmd of
-  New          -> Model (M.insert resp 0 m)
-  Read  _      -> Model m
-  Write ref i  -> Model (M.insert ref i m)
-  Inc   ref    -> Model (M.insert ref (m M.! ref + 1) m)
-  Copy  ref    -> Model (M.insert resp (m M.! ref) m)) \\ (iinstF @'() Proxy :: Ords refs)
+  New         -> Model (M.insert resp 0 m)
+  Read  _     -> Model m
+  Write ref i -> Model (M.insert ref i m)
+  Inc   ref   -> Model (M.insert ref (m M.! ref + 1) m)
+  Copy  ref   -> Model (M.insert resp (m M.! ref) m)) \\ (iinstF @'() Proxy :: Ords refs)
 
 postconditions :: forall refs resp. IxForallF Ord refs => Model refs -> MemStep resp refs
   -> Response_ refs resp -> Property
@@ -203,149 +190,3 @@ prop_parallel prb = parallelProperty
   shrink1
   returns
   (semStep prb)
-
-------------------------------------------------------------------------
-
-usesRefs :: MemStep resp refs -> [Ex refs]
-usesRefs New           = []
-usesRefs (Read  ref)   = [Ex STuple0 ref]
-usesRefs (Write ref _) = [Ex STuple0 ref]
-usesRefs (Inc   ref)   = [Ex STuple0 ref]
-usesRefs (Copy  ref)   = [Ex STuple0 ref]
-
-scopeCheck
-  :: forall
-     (ix   :: *)
-     (cmd  :: Response ix -> (TyFun ix * -> *) -> *)
-  .  (forall resp. cmd resp ConstIntRef -> SResponse ix resp)
-  -> (forall resp. cmd resp ConstIntRef -> [Ex ConstIntRef])
-  -> [(Pid, IntRefed cmd)]
-  -> Bool
-scopeCheck returns' uses' = go []
-  where
-  go :: [IntRef] -> [(Pid, IntRefed cmd)] -> Bool
-  go _    []                           = True
-  go refs ((_, Untyped' c miref) : cs) = case returns' c of
-    SReference _  ->
-      let refs' = miref : refs in
-      all (\(Ex _ ref) -> ref `elem` refs) (uses' c) &&
-      go refs' cs
-    SResponse     ->
-      all (\(Ex _ ref) -> ref `elem` refs) (uses' c) &&
-      go refs cs
-
-scopeCheckFork'
-  :: forall
-     (ix   :: *)
-     (cmd  :: Response ix -> (TyFun ix * -> *) -> *)
-  .  (forall resp. cmd resp ConstIntRef -> SResponse ix resp)
-  -> (forall resp. cmd resp ConstIntRef -> [Ex ConstIntRef])
-  -> Fork [IntRefed cmd] -> Bool
-scopeCheckFork' returns' uses' (Fork l p r) =
-  let p' = zip (repeat 0) p in
-  scopeCheck returns' uses' (p' ++ zip (repeat 1) l) &&
-  scopeCheck returns' uses' (p' ++ zip (repeat 2) r)
-
-scopeCheckFork :: Fork [Untyped' MemStep ConstIntRef] -> Bool
-scopeCheckFork = scopeCheckFork' returns usesRefs
-
-prop_genScope :: Property
-prop_genScope = forAll (fst <$> liftGen gens (Pid 0) M.empty returns) $ \p ->
-  let p' = zip (repeat 0) p in
-  scopeCheck returns usesRefs p'
-
-prop_genForkScope :: Property
-prop_genForkScope = forAll
-  (liftGenFork gens returns)
-  scopeCheckFork
-
-prop_sequentialShrink :: Property
-prop_sequentialShrink = shrinkPropertyHelper (prop_safety Bug) $ alphaEq returns
-  [ Untyped' New    (IntRef (Ref 0) (Pid 0))
-  , Untyped' (Write (IntRef (Ref 0) (Pid 0)) (5)) ()
-  , Untyped' (Read  (IntRef (Ref 0) (Pid 0))) ()
-  ]
-  . read . (!! 1) . lines
-
-deriving instance Eq  (MemStep resp ConstIntRef)
-
-instance Eq (Untyped' MemStep ConstIntRef) where
-  Untyped' c1 _ == Untyped' c2 _ = Just c1 == cast c2
-
-cheat :: Fork [Untyped' MemStep (ConstSym1 refs)] -> Fork [Untyped' MemStep (ConstSym1 refs)]
-cheat = fmap (map (\ms -> case ms of
-  Untyped' (Write ref _) () -> Untyped' (Write ref 0) ()
-  _                         -> ms))
-
-prop_shrinkForkSubseq :: Property
-prop_shrinkForkSubseq = forAll (liftGenFork gens returns) $ \f@(Fork l p r) ->
-  all (\(Fork l' p' r') -> noRefs l' `isSubsequenceOf` noRefs l &&
-                           noRefs p' `isSubsequenceOf` noRefs p &&
-                           noRefs r' `isSubsequenceOf` noRefs r)
-      (liftShrinkFork returns shrink1 (cheat f))
-
-  where
-  noRefs = fmap (const ())
-
-prop_shrinkForkScope :: Property
-prop_shrinkForkScope = forAll (liftGenFork gens returns) $ \f ->
-  all scopeCheckFork (liftShrinkFork returns shrink1 f)
-
-debugShrinkFork :: Fork [Untyped' MemStep ConstIntRef]
-  -> [Fork [Untyped' MemStep ConstIntRef]]
-debugShrinkFork = take 1 . map snd . dropWhile fst . map (\f -> (scopeCheckFork f, f))
-  . liftShrinkFork returns shrink1
-
-------------------------------------------------------------------------
-
-prop_shrinkForkMinimal :: Property
-prop_shrinkForkMinimal = shrinkPropertyHelper (prop_parallel RaceCondition) $ \out ->
-  let f = read $ dropWhile isSpace (lines out !! 1)
-  in hasMinimalShrink f ||  isMinimal f
-  where
-  hasMinimalShrink :: Fork [Untyped' MemStep ConstIntRef] -> Bool
-  hasMinimalShrink
-    = anyRose isMinimal
-    . rose (liftShrinkFork returns shrink1)
-    where
-    anyRose :: (a -> Bool) -> Rose a -> Bool
-    anyRose p (Rose x xs) = p x || any (anyRose p) xs
-
-    rose :: (a -> [a]) -> a -> Rose a
-    rose more = go
-      where
-      go x = Rose x $ map go $ more x
-
-  isMinimal :: Fork [Untyped' MemStep ConstIntRef] -> Bool
-  isMinimal xs = any (alphaEqFork returns xs) minimal
-
-  minimal :: [Fork [Untyped' MemStep ConstIntRef]]
-  minimal  = minimal' ++ map mirrored minimal'
-    where
-    minimal' = [ Fork [w0, Untyped' (Read var) ()]
-                      [Untyped' New var]
-                      [w1]
-               | w0 <- writes
-               , w1 <- writes
-               ]
-
-    mirrored :: Fork a -> Fork a
-    mirrored (Fork l p r) = Fork r p l
-
-    var = IntRef 0 0
-    writes = [Untyped' (Write var 0) (), Untyped' (Inc var) ()]
-
-instance Read (Untyped' MemStep ConstIntRef) where
-  readPrec = parens $ choice
-    [ Untyped' <$ key "Untyped'" <*> parens (New <$ key " New") <*> readPrec
-    , Untyped' <$ key "Untyped'" <*>
-        parens (Read <$ key "Read" <*> readPrec) <*> readPrec
-    , Untyped' <$ key "Untyped'" <*>
-        parens (Write <$ key "Write" <*> readPrec <*> readPrec) <*> readPrec
-    , Untyped' <$ key "Untyped'" <*>
-        parens (Inc <$ key "Inc" <*> readPrec) <*> readPrec
-    ]
-    where
-      key s = Text.Read.lift (string s)
-
-  readListPrec = readListPrecDefault
