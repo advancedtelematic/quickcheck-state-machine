a213c0c38d33d3bf43cb5fd53fc2b91d280165c4
lyxia <lysxia@gmail.com>
2017-09-29 08:00:16 -0400

    Add hspec module for CircularBuffer

---
+++ example/test/CircularBufferSpec.hs
@@ -0,0 +1,30 @@
+module CircularBufferSpec (spec) where
+
+import           Test.Hspec
+                   (Spec, describe, it)
+import           Test.Hspec.QuickCheck
+import           Test.QuickCheck
+                   (expectFailure)
+
+import           CircularBuffer
+
+spec :: Spec
+spec = do
+  modifyMaxSuccess (const 500) $
+
+    describe "Sequential property" $ do
+
+      it "`unpropNoSizeCheck`: the first bug is found" $
+        expectFailure unpropNoSizeCheck
+
+      it "`unpropFullIsEmpty`: the second bug is found" $
+        expectFailure unpropFullIsEmpty
+
+      it "`unpropBadRem`: the third bug is found" $
+        expectFailure unpropBadRem
+
+      it "`unpropStillBadRem`: the fourth bug is found" $
+        expectFailure unpropStillBadRem
+
+      it "`prop_circularBuffer`: the fixed version is correct" $
+        prop_circularBuffer
