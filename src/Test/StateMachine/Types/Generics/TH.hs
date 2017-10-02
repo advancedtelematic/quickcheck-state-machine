95e4a98560a64ca3c9d8078977730d2029a074f4
lyxia <lysxia@gmail.com>
2017-10-01 20:18:55 -0400

    Add generic shrinker with TH

--- src/Test/StateMachine/Types/Generics/TH.hs
+++ src/Test/StateMachine/Types/Generics/TH.hs
@@ -3,15 +3,76 @@
 module Test.StateMachine.Types.Generics.TH where
 
 import           Control.Monad
-                   ((>=>))
+                   (filterM, (>=>))
 import           Data.Foldable
                    (foldl')
 import           Language.Haskell.TH
 import           Language.Haskell.TH.Datatype
+import           Test.QuickCheck
+                   (shrink)
 
 import           Test.StateMachine.Internal.Utils
-                   (dropLast)
+                   (dropLast, nub)
 import           Test.StateMachine.Types.Generics
+import           Test.StateMachine.Types.References
+                   (Reference)
+
+-- * Shrinkers
+
+-- | A generic shrinker which avoids shrinking references.
+mkShrinker :: Name -> Q Exp
+mkShrinker = reifyDatatype >=> mkShrinker'
+
+mkShrinker' :: DatatypeInfo -> Q Exp
+mkShrinker' info = do
+  x <- newName "x"
+  tms <- traverse shrinkerMatches (datatypeCons info)
+  let (_ts, ms) = unzip tms
+  lamE [varP x] (caseE (varE x) ms)
+
+shrinkerMatches :: ConstructorInfo -> Q ([Type], Q Match)
+shrinkerMatches info = do
+  xts <- traverse (\t -> (,) <$> newName "x" <*> pure t) (constructorFields info)
+  yts <- filterM (\(_, t) -> shrinkable t) xts
+  let (ys, ts) = unzip yts
+      fieldPats | [] <- ys = [wildP | _ <- xts]
+                 | otherwise = [varP x | (x, _) <- xts]
+      m = match (conP (constructorName info) fieldPats) (normalB body) []
+      e = foldl' appE (conE (constructorName info)) [varE x | (x, _) <- xts]
+      body | [] <- ys = listE []  -- No field is shrinkable
+           | otherwise = [|fmap|]
+               `appE` lamE [listTupleP ys] e
+               `appE` [|shrink $(listTupleE ys)|]
+  return (nub ts, m)
+
+listTupleP :: [Name] -> PatQ
+listTupleP = listTuple unit cons . fmap varP
+  where
+    unit = conP (tupleDataName 0) []
+    cons a b = tupP [a, b]
+
+listTupleE :: [Name] -> ExpQ
+listTupleE = listTuple unit cons . fmap varE
+  where
+    unit = conE (tupleDataName 0)
+    cons a b = tupE [a, b]
+
+listTuple :: a -> (a -> a -> a) -> [a] -> a
+listTuple nil cons = go
+  where
+    go []       = nil
+    go [a]      = a
+    go (a : as) = cons a (go as)
+
+shrinkable :: Type -> Q Bool
+shrinkable =
+  fmap (not . isReference) . resolveTypeSynonyms
+
+isReference :: Type -> Bool
+isReference (AppT (AppT (ConT r) _) _) = r == ''Reference
+isReference _                          = False
+
+-- * Constructor class
 
 deriveConstructors :: Name -> Q [Dec]
 deriveConstructors = (fmap . fmap) deriveConstructors' reifyDatatype
